aaf15fa789851d34c18a4d9d99c402bc769b03f1
huise0ng <shinhuiseong07@gmail.com>
2024-01-25 10:10:40 +0900

    FIX :: Pixel

--- pixel.js
+++ pixel.js
@@ -1,15 +1,30 @@
 class Pixel {
-  constructor(r, g, b, x, y) {
-    this.r = r
-    this.g = g
-    this.b = b
-    this.x = x
-    this.y = y
+  constructor(colors, x, y) {
+    this.colors = colors;
+    this.x = x;
+    this.y = y;
+    this.sx = random(400); 
+    this.sy = random(400);
+    this.tx = x * 5; 
+    this.ty = y * 5;
+    this.speed = random(0.08, 0.1); 
   }
-  
+
   show() {
-    noStroke()
-    fill(this.r, this.g, this.b);
-    ellipse(random(4000), random(4000), 5, 5);
+    noStroke();
+    fill(this.colors);
+    ellipse(this.sx, this.sy, 5, 5);
+  }
+
+  move() {
+    this.sx = lerp(this.sx, this.tx, this.speed);
+    this.sy = lerp(this.sy, this.ty, this.speed);
+  }
+
+  bounceRandomly() {
+    let angle = random(TWO_PI); 
+    let distance = random(10, 50); 
+    this.tx = this.sx + cos(angle) * distance;
+    this.ty = this.sy + sin(angle) * distance;
   }
 }
