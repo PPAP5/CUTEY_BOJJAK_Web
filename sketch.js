6db976fde16aef81b6eeb6a83207d14af0448313
huise0ng <shinhuiseong07@gmail.com>
2024-01-25 10:11:34 +0900

    ADD :: 절대 닿이지 않아요

--- sketch.js
+++ sketch.js
@@ -1,26 +1,30 @@
 let img;
+let pixels = [];
 
 function preload() {
-  img = loadImage('홍진호.png'); // 이미지를 미리 로드합니다.
+  img = loadImage('홍진호.png'); 
 }
 
-let a = 6
-
 function setup() {
-  createCanvas(4000, 4000);
-  
-  
-  
+  createCanvas(400, 400);
+
   for (let i = 0; i < img.width; ++i) {
     for (let j = 0; j < img.height; ++j) {
-      let pixelColor = img.get(i,j);
-      
-      
-    }  
+      pixels.push(new Pixel(img.get(i, j), i, j));
+    }
   }
 }
 
-function createPixel(x, y, r, g, b) {
-  fill(r, g, b)
-  rect(x, y, 5)
+function draw() {
+  background(0);
+
+  for (let p of pixels) {
+    p.show();
+    p.move();
+    
+    if ((p.tx+5 >= mouseX && p.tx-5 <= mouseX) && (p.ty+5 >= mouseY && p.ty-5 <= mouseY)) {
+      p.bounceRandomly()
+    }
+    
+  }
 }
